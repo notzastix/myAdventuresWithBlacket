4b6980e55237a46b221f91e095351e46192cf395
zastix <69748767+notzastix@users.noreply.github.com>
2022-06-12 16:04:55 -0500

    Create database.js

---
+++ src/multitool/database.js
@@ -0,0 +1,69 @@
+const Database = require("@replit/database")
+const db = new Database()
+db.set('users', {
+  'qaiik': {
+    perms: 'Customer',
+    pos: 40,
+    pfp: ''
+  },
+  'penguinpowers': {
+    perms: 'Admin',
+    pos: 37,
+    pfp: 'https://cdn.discordapp.com/avatars/778008377803931670/97a74f51583b2d0867b92ef9e6d6eb20.webp'
+  },
+  'Xotic': {
+    perms: 'Sexy',
+    pos: 40,
+    pfp: ''
+  },
+  'Blacket': {
+    perms: 'Customer',
+    pos: 40,
+    pfp: 'https://discord.com/assets/6f26ddd1bf59740c536d2274bb834a05.png'
+  },
+  'gcguesty': {
+    perms: 'Customer',
+    pos: 40,
+    pfp: 'https://media.discordapp.net/attachments/955234978524962826/965405255355494420/20210810_062806.jpg?width=401&height=401'
+  },
+  'joemom': {
+    perms: 'Customer',
+    pos: 40,
+    pfp: ''
+  },
+  'zastixalt': {
+    perms: 'Owner',
+    pos: 42,
+    pfp: 'https://cdn.discordapp.com/avatars/716899506460950539/4241789c3b478c74e90ec277000022d2.webp'
+  },
+  'zastix': {
+    perms: 'Owner',
+    pos: 42,
+    pfp: 'https://cdn.discordapp.com/avatars/716899506460950539/4241789c3b478c74e90ec277000022d2.webp'
+  },
+  'BlaxKingYT': {
+    perms: 'Customer',
+    pos: 40, // nts: do this
+    pfp: 'https://cdn.discordapp.com/avatars/761588152414502932/7b61a91eee58c515a2add05c149b950d.webp?size=80'
+  },
+  'Fristic': {
+    perms: 'Customer',
+    pos: 40, // note to self: do this
+    pfp: 'https://cdn.discordapp.com/avatars/872579810914889728/f689701591f16d3f4732bb6d1fb0b027.webp'
+  },
+  'tremblero': {
+    perms: 'scripter',
+    pos: 40,
+    pfp: 'https://cdn.discordapp.com/avatars/553776059363885066/a_74b12b8b0027839b6323542123ef79d1.webp'
+  },
+  'GAYBALLS': {
+    perms: 'gay',
+    pos: 40,
+    pfp: ''
+  },
+  'KINGBLOOK': {
+    perms: 'Customer',
+    pos: 39,
+    pfp: 'https://cdn.discordapp.com/avatars/716899506460950539/4241789c3b478c74e90ec277000022d2.webp'
+  }
+});
